65d149d87ee19a182355905a1053b989c1b15c54
Qianyun Guo <qianyunguo@gmail.com>
2017-04-25 13:54:47 +0200

    ac

--- poj1014.cpp
+++ poj1014.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstring> 
 #include <algorithm> 
+
 using namespace std;
 
 int a[10];
@@ -9,29 +10,33 @@ int b[10];
 int total;
 
 bool dfs(int v, int mx) {
-    if (v == total)
+    if (v == 0)
         return true;
+    if (b[mx] < v) 
+        return false;
     
-    for (int i = mx; i > 0; i--){
-        if (a[i]) {
-            if (v + i <= total) {
-                a[i] --;
-                return dfs(v + i, i);
-            }
+    for (int i = min(v / mx, a[mx]); i >= 0; i--){
+        //cout << "mx: " << mx << "number: " << i << "value: " << v << endl;
+        if (i*mx + b[mx-1] < v)
+            break;
+        if (dfs(v - i*mx, mx-1)){
+            return true;
         }
     }
     return false;
 }
 
 int main() {
-    for (int i = 1; i <= 6; i++) 
-        b[i] = i;
     int tt = 1;
     while (1) {
         total = 0;
+        //memset(b, 0, sizeof(b));
+        for (int i=0; i < 10; i++)
+            b[i] = 0;
         for (int i = 1; i <= 6; i++) {
             cin >> a[i];
             total += a[i] * i;
+            b[i] += total;
         }
         if (total == 0) break;
         cout << "Collection #" << tt++ << ":\n";
@@ -40,10 +45,8 @@ int main() {
             continue;
         }
         total /= 2;
-        cout << (dfs(0,6) ? "Can be divided.\n" : "Can't be divided.\n") << endl;
+        //cout << total << endl;
+        cout << (dfs(total, 6) ? "Can be divided.\n" : "Can't be divided.\n") << endl;
     }
-
-    
-    
     return 0;
 }
